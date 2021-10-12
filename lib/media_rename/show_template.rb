cf2b3b2f18f968bbe97ff115408afed5845b92a9
John Tajima <manjiro@gmail.com>
2021-10-12 19:03:11 -0400

    soemtimes year doesn't show - temp fix

--- lib/media_rename/show_template.rb
+++ lib/media_rename/show_template.rb
@@ -8,7 +8,7 @@ module MediaRename
       @media      = media
       @attributes = {
         title: record.show_title.to_s.gsub(':', '-'),
-        year: record.year,
+        year: (record.year rescue ''),
         video_format: MediaRename::Media.video_format(media.width, media.height),
         video_codec: MediaRename::Media.video_codec(media.video_codec),
         audio_codec: MediaRename::Media.audio_codec(media.audio_codec, media.audio_channels),
