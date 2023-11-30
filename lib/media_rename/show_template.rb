61e3fdb0cc186820bb82a7e29b81f9f301a458e3
John Tajima <manjiro@gmail.com>
2023-11-30 08:51:06 -0500

    move hq tv shows into their own folder

--- lib/media_rename/show_template.rb
+++ lib/media_rename/show_template.rb
@@ -16,7 +16,7 @@ module MediaRename
       {
         title: record.show_title.to_s.gsub(':', '-'),
         year: (record.year rescue ''),
-        video_format: MediaRename::Media.video_format(media.width, media.height, media.video_codec),
+        video_format: MediaRename::Media.video_format(media.width, media.height),
         video_codec: video_codec,
         audio_codec: MediaRename::Media.audio_codec(media.audio_codec, media.audio_channels, atmos?, ddplus?),
         tags: "",
